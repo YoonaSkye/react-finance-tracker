c7b981dd8d1384ff6a89cdfcc6d61d0ce8537ebe
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-01-02 20:44:32 +0800

    create signup form

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+
 // styles
-import stles from "./Signup.module.css";
+import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form className={styles["signup-form"]} onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }
