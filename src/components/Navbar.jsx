11ac48a5560207fa7e812da181399e0b8aaf4728
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-01-04 16:39:30 +0800

    Firebase Auth state change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // styles
 import styles from "./Navbar.module.css";
@@ -7,6 +8,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
@@ -14,17 +16,26 @@ export default function Navbar() {
         <li className={styles.title}>
           <Link to="/">Finace Tracker</Link>
         </li>
-        <li>
-          <Link to="/login">Login </Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login </Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <>
+            <li>hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
