2a0352707d8896fab39789d260f844daba947cb5
Sheldon Liao <sheldon.liao1015@gmail.com>
2022-01-04 20:11:43 +0800

    create TransactionList and fetch realtime data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,23 @@
-import TransactionForm from "./TransactionForm";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+
+// import components
+import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 // styles
 import styles from "./Home.module.css";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { error, documents } = useCollection("transaction");
+
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
